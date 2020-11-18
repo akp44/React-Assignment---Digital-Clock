969808c3988da215b67625678e63770b51a1190d
akshay pandey <kumarakshaypandey@gmail.com>
2020-11-18 21:12:10 +0530

    updating hour....

--- src/components/App.js
+++ src/components/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         ];
         const amOrPm = hours >= 12 ? "PM" : "Am";
         const twelveHourFormat = hours > 12 ? hours - 12 : hours;
-        const hourString = this.padNumberToTwoDigits(twelveHourFormat);
+        const hourString = '' + twelveHourFormat;
         const minuteString = this.padNumberToTwoDigits(minutes);
         const secondString = this.padNumberToTwoDigits(seconds);
 
